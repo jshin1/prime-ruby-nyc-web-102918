6d35e61c125e9f5d5752ea6358b49543799fa38a
Ji S <j2yshin@gmail.com>
2018-10-18 21:18:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -13,3 +13,17 @@ def prime?(integer)
     end
   end
 end
+
+def prime?(integer)
+  if integer > 1
+    array_of_numbers = (2..(integer - 1)).to_a
+    array_of_numbers.each do |i|
+      if integer % i == 0
+        false
+      else
+        true
+      end
+    end
+  else
+    false
+end
