bd6281f650348cc79a08348a66cf931c7cd9d39c
Ji S <j2yshin@gmail.com>
2018-10-18 20:50:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(integer)
-  if integer <= 1
+  if integer < 2
     false
   else
-    new_array = (2..(integer ** (1/2))).to_a 
-    new_array each {|number| false if integer % number == 0}
-    true
+    new_array = (2..(integer - 1)).to_a 
+    new_array.each do |number| 
+    false if integer % number == 0}
   end
 end
