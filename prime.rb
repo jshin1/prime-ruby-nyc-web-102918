d423279b5497468708237fd2b41ab6f08f2cd670
Ji S <j2yshin@gmail.com>
2018-10-18 20:48:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(integer)
+  if integer <= 1
+    false
+  else
+    new_array = (2..(integer ** (1/2))).to_a 
+    new_array each {|number| false if integer % number == 0}
+    true
+  end
+end
