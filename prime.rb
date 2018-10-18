ee33fca2d233a0d5562192225df213d0520915a3
Ji S <j2yshin@gmail.com>
2018-10-18 21:23:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,11 @@ def prime?(integer)
   if integer < 2
     false
   end
-    new_array = (2..(integer - 1)).to_a 
-    new_array.collect do |i| 
-      if integer % i == 0
-        false
-      end
-      true
+  new_array = (2..(integer - 1)).to_a 
+  new_array.collect do |i| 
+    if integer % i == 0
+      false
     end
+    true
   end
 end
